3e9859ea7b883d929081f786492b39b9f19b5742
loudringphone <loudringphone@gmail.com>
2023-03-13 21:51:40 +1100

    third commit

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://dtrjaardnfrqhkaleiwu.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0cmphYXJkbmZycWhrYWxlaXd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxODE3MjgsImV4cCI6MTk5Mzc1NzcyOH0.qE4alcF7wOgnSI-ySAcCOA0rbkwEzFn4E3h7nmKXW0E"
-);
+
+const SUPABASE_URL = process.env.VUE_APP_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.VUE_APP_SUPABASE_ANON_KEY;
+
+console.log(process.env.VUE_APP_SUPABASE_URL)
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
